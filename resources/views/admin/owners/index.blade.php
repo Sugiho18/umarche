28030552b085bcf3ca5d5689c4d51627343fe934
Sugiho18 <sugifune@se-sendai.co.jp>
2023-10-19 13:27:32 +0900

    フラッシュメッセージ

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font">
                           <div class="flex flex-col text-center w-full">
-                          </div>
+                            <x-flash-message status="info"/>
                           <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                             <table class="table-auto w-full text-left whitespace-no-wrap">
                             <div class="flex justify-end mb-4">
