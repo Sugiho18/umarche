ae01e28a218b5a35fd2ad939105dbbb39968927f
Sugiho18 <sugifune@se-sendai.co.jp>
2023-10-26 16:59:39 +0900

    107.Product Create

--- database/migrations/2023_10_26_101419_create_products_table.php
+++ database/migrations/2023_10_26_101419_create_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\Stmt\TraitUseAdaptation\Precedence;
 
 class CreateProductsTable extends Migration
 {
@@ -15,6 +16,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')
+            ->nullable();
             $table->foreignId('shop_id')
             ->constrained()
             ->onUpdate('cascade')
@@ -22,6 +29,15 @@ class CreateProductsTable extends Migration
             $table->foreignId('secondary_category_id')
             ->constrained();
             $table->foreignId('image1')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image2')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image3')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image4')
             ->nullable()
             ->constrained('images');
             $table->timestamps();
